8d655d48c6aa23ad6d3242411b9425e111562808
Simone Cimarelli <aquilairreale@ymail.com>
2018-08-31 18:34:56 +0200

    Improve pprintf and move it in a new module

---
+++ src/pprintf.c
@@ -0,0 +1,107 @@
+#include <unistd.h>
+#include <termios.h>
+#include <sys/ioctl.h>
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdarg.h>
+#include <string.h>
+#include <assert.h>
+
+#include "pprintf.h"
+
+static unsigned short pprintf_h;
+static unsigned short pprintf_w;
+static unsigned short pprintf_r;
+static unsigned short pprintf_c;
+
+void pprintf_init()
+{
+    struct winsize ws;
+    ioctl(STDIN_FILENO, TIOCGWINSZ, &ws);
+
+    pprintf_h = ws.ws_row;
+    pprintf_w = ws.ws_col;
+    pprintf_r = 0;
+    pprintf_c = 0;
+}
+
+static void pprintf_block()
+{
+    fputs(PPRINTF_PROMPT, stdout);
+
+    struct termios flags;
+    tcgetattr(STDIN_FILENO, &flags);
+
+    flags.c_lflag &= ~ECHO;
+    tcsetattr(STDIN_FILENO, TCSANOW, &flags);
+
+    while (getchar() != '\n');
+
+    flags.c_lflag |= ECHO;
+    tcsetattr(STDIN_FILENO, TCSANOW, &flags);
+
+    size_t i;
+    for (i = 0; i < strlen(PPRINTF_PROMPT); i++) {
+        fputs("\b \b", stdout);
+    }
+
+    putchar('\r');
+
+    pprintf_init();
+}
+
+int pprintf(const char *format, ...)
+{
+    va_list ap;
+
+    va_start(ap, format);
+    int len = vsnprintf(NULL, 0, format, ap);
+    va_end(ap);
+
+    char *buf = malloc((len + 1) * sizeof *buf);
+
+    va_start(ap, format);
+    vsprintf(buf, format, ap);
+    va_end(ap);
+
+    char *line = buf;
+    char *end;
+
+    do {
+        end = strchr(line, '\n');
+
+        if (end != NULL) {
+            *end = '\0';
+        }
+
+        do {
+            if (pprintf_r + 1 >= pprintf_h && line[0] != '\0') {
+                pprintf_block();
+            }
+
+            assert(pprintf_c < pprintf_w);
+            short space_left = pprintf_w - pprintf_c;
+
+            unsigned short written = printf("%.*s", space_left, line);
+            if (written == space_left || end != NULL) {
+                if (end != NULL) {
+                    putchar('\n');
+                }
+
+                pprintf_c = 0;
+                pprintf_r++;
+            } else {
+                pprintf_c += written;
+            }
+
+            line += written;
+        } while (line[0] != '\0');
+
+        line = end + 1;
+    } while (end != NULL);
+
+    free(buf);
+
+    return len;
+}
---
+++ src/pprintf.h
@@ -0,0 +1,9 @@
+#ifndef _PPRINTF_H_
+#define _PPRINTF_H_
+
+#define PPRINTF_PROMPT "--MORE--"
+
+void pprintf_init();
+int pprintf(const char *format, ...);
+
+#endif /* _PPRINTF_H_ */
